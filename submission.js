66b34806540a32bfca5e71b2aa64f0b1363b4a9a
Evan Hahn <me@evanhahn.com>
2014-02-23 18:34:39 -0500

    begin the submission

--- submission.js
+++ submission.js
@@ -1,1 +1,15 @@
 // jshint ignore: start
+
+S = 64; // how big is the dragon?
+W = a.width / 2;
+H = a.height / 2;
+
+(function t() {
+
+	c.beginPath();
+	c.arc(W, H, 64, 0, 7);
+	c.fill();
+
+	requestAnimationFrame(t);
+
+})();
