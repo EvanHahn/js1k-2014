746e9204881f4b500ef7b95132929e3e44f35433
Evan Hahn <me@evanhahn.com>
2014-03-02 09:58:39 -1000

    mostly done! time to polish

--- submission.js
+++ submission.js
@@ -1,41 +1,153 @@
+// lowercases are global VALUES
+// uppercases are global FUNCTIONS
+// local values are t, T, i, j, k
+
 // jshint ignore: start
 
-P = (M = Math).PI; // M = Math, P = PI
+// -- globals and methods and shorthands --
 
-S = 64; // how big is the dragon?
+a = new AudioContext;
 
-w = (W = a.width) / 2;
-h = (H = a.height) / 2;
+d(); // remove canvas
+d = document;
 
-l = 0.1; // wing curl amount
+HTMLElement.prototype.A = function(element) {
+	this.appendChild(element);
+};
 
-(function t() {
+function C(tagName) {
+	return d.createElement(tagName);
+}
 
-	with (c) {
+function P(f, n, g, o) { // play a note with frequency
+	n = a.currentTime;
+	g = a.createGain();
+	with (g) {
+		with (gain) {
+			setValueAtTime(1, n);
+			linearRampToValueAtTime(1, n);
+			linearRampToValueAtTime(0, n + 1);
+		}
+		connect(a.destination);
+	}
+	o = a.createOscillator();
+	with (o) {
+		type = 0;
+		frequency.value = f;
+		connect(g);
+		noteOn(n);
+		noteOff(n + 1);
+	}
+	setTimeout(function() {
+		o.disconnect(g);
+		g.disconnect(a.destination);
+	}, 1000);
+}
 
-		clearRect(0, 0, W, H);
+l = 'table';
+y = 'style';
 
-		fillStyle = '#090';
+ff = '#fff';
+oo = '#000';
 
-		beginPath();
-		moveTo(w, h);
-		quadraticCurveTo(w - S, h + S, w - S * 2, h - S);
-		arc(w, h - S, S, P * (1 - l), P * l, true);
-		lineTo(w + S * 2, h - S);
-		quadraticCurveTo(w + S, h + S, w, h);
-		fill();
+n = [
+	123.47,
+	196,
+	246.94,
+	392,
+	493.88,
+	783.99,
+	987.77,
+	1567.98
+];
 
-		save();
-		translate(w, h);
-		scale(1, 2);
-		beginPath();
-		arc(w, h, S, 0, 7);
-		restore();
-		fillStyle = '#000000';
-		fill();
+r = [];
+
+// -- add the elements to the DOM --
+
+(T = C(l)).id = 'p'; // <table id="p">
+b.A(T); // add table to body
+for (i = 8; i --;) {
+
+	k = [];
+
+	T = C('tr');
+
+	for (j = 8; j --;) {
+
+		// make a <td>
+		(t = C('td')).f = n[i];
+		t.width = t.height = 64;
+		t.o = Math.random() > .2;
+		t[y].transition = 'background-color .1s ease';
+
+		// toggle background and status on click
+		(t.onclick = function() {
+			with (this) {
+				o = !o;
+				className = o ? 'o' : '';
+			}
+		}).call(t);
+
+		T.A(t); // add column to row
+		k.push(t);
 
 	}
 
-	requestAnimationFrame(t);
+	p.A(T); // add row to table
+	r.push(k);
 
-})();
+}
+
+// -- styles --
+
+i = '100%';
+with (d.documentElement[y]) {
+	display = l;
+	width = height = i;
+	background = '#222';
+}
+with (b[y]) {
+	display = l + '-cell';
+	width = height = i;
+	verticalAlign = 'middle';
+	fontFamily = 'sans-serif';
+}
+with (p[y]) {
+	margin = '0 auto';
+	background = ff;
+	boxShadow = '0 0 20px' + ff + ',0 0 10px' + oo;
+}
+
+// -- play that funky music --
+
+b = -1;
+k = 'background';
+setInterval(function() {
+
+	b = (b + 1) % 8;
+
+	for (i = 8; i --;) {
+		for (j = 8; j --;) {
+
+			t = r[i][j];
+
+			if (j == b) { // it's the current beat for this one
+				if (t.o) {
+					P(t.f);
+					t[y][k] = '#f34';
+				} else {
+					t[y][k] = '#200';
+				}
+			} else {
+				if (t.o) {
+					t[y][k] = ff;
+				} else {
+					t[y][k] = oo;
+				}
+			}
+
+		}
+	}
+
+}, 500);
