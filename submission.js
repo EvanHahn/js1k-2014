2f06a30cfe50a70a92ca01cd20048e82348d785e
Evan Hahn <me@evanhahn.com>
2014-03-02 07:52:23 -1000

    dragon wings

--- submission.js
+++ submission.js
@@ -15,7 +15,7 @@ l = 0.1; // wing curl amount
 
 		clearRect(0, 0, W, H);
 
-		c.fillStyle = '#090';
+		fillStyle = '#090';
 
 		beginPath();
 		moveTo(w, h);
@@ -23,6 +23,15 @@ l = 0.1; // wing curl amount
 		arc(w, h - S, S, P * (1 - l), P * l, true);
 		lineTo(w + S * 2, h - S);
 		quadraticCurveTo(w + S, h + S, w, h);
+		fill();
+
+		save();
+		translate(w, h);
+		scale(1, 2);
+		beginPath();
+		arc(w, h, S, 0, 7);
+		restore();
+		fillStyle = '#000000';
 		fill();
 
 	}
